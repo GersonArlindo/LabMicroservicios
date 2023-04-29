33df26caf5b32ae2d885e2a01d7cbaaad89659ff
GersonArlindo <gerson50039@gmail.com>
2023-04-29 09:04:18 -0600

    Ejercicio 3 Finalizado

--- microservices/perros-service/routes/perros/index.js
+++ microservices/perros-service/routes/perros/index.js
@@ -8,16 +8,87 @@ const router = express.Router();
 
 const logger = (message) => console.log(`Perros Service: ${message}`);
 
+//GET PERROS
+// router.get("/", (req, res) => {
+//   const response = {
+//     service: "perros",
+//     architecture: "microservices",
+//     length: data.length,
+//     data: data,
+//   };
+//   logger("Get perros data");
+//   return res.send(response);
+// });
+
+//Ejercicio 1
 router.get("/", (req, res) => {
+  const { pais, peso, op } = req.query;
+  if (!pais && !peso) {
+    return res.status(400).send("Se debe proporcionar al menos un filtro");
+  }
+  let query = "SELECT * FROM perros";
+  const params = [];
+  if (pais) {
+    query += " WHERE pais_dueno = ?";
+    params.push(pais);
+  }
+  if (peso) {
+    if (!op) {
+      return res.status(400).send("Se debe proporcionar un operador para el peso");
+    }
+    const pesoNum = parseFloat(peso);
+    if (isNaN(pesoNum)) {
+      return res.status(400).send("El peso debe ser un número válido");
+    }
+    switch (op) {
+      case ">":
+        query += " WHERE peso > ?";
+        break;
+      case "<":
+        query += " WHERE peso < ?";
+        break;
+      case ">=":
+        query += " WHERE peso >= ?";
+        break;
+      case "<=":
+        query += " WHERE peso <= ?";
+        break;
+      case "=":
+        query += " WHERE peso = ?";
+        break;
+      default:
+        return res.status(400).send("El operador proporcionado no es válido");
+    }
+    params.push(pesoNum);
+  }
+
+  db.all(query, params, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Error interno del servidor");
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+
+//GET PERRO BY ID
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const perro = data.find((p) => p.Id == id);
+  if (!perro) {
+    return res.status(404).send("Perro no encontrado");
+  }
   const response = {
     service: "perros",
     architecture: "microservices",
-    length: data.length,
-    data: data,
+    data: perro,
   };
-  logger("Get perros data");
+  logger(`Get perro con id ${id}`);
   return res.send(response);
 });
+
 
 // Exportamos el router
 module.exports = router;
--- microservices/premios-service/routes/premios/index.js
+++ microservices/premios-service/routes/premios/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require('axios');
 const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 
@@ -16,4 +17,197 @@ router.get("/", (req, res) => {
     }
   });
 }); 
+
+//Ejercicio 3
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const query = "SELECT * FROM campeonatos WHERE id = ?";
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Error interno del servidor");
+    } else if (!row) {
+      res.status(404).send("Campeonato no encontrado");
+    } else {
+      // Obtener id_campeon del objeto row
+      const id_campeon = row.id_campeon;
+      
+      // Hacer la solicitud a la API de perros usando el id_campeon
+      axios
+        .get(`http://localhost:4000/api/v2/perros/${id_campeon}`)
+        .then(response => {
+          // Combinando
+          const data = {
+            id: row.id,
+            id_campeon: row.id_campeon,
+            anio_campeonato: row.anio_campeonato,
+            lugar: row.lugar,
+            categoria_ganada: row.categoria_ganada,
+            pais_competencia: row.pais_competencia,
+            premio: row.premio,
+            puntaje: row.puntaje,
+            perro: response.data.data
+          };
+          res.json(data);
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).send("Error interno del servidor");
+        });
+    }
+  });
+});
+
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const query = "SELECT * FROM campeonatos WHERE id = ?";
+//   db.get(query, [id], (err, row) => {
+//     if (err) {
+//       console.error(err.message);
+//       res.status(500).send("Error interno del servidor");
+//     } else if (!row) {
+//       res.status(404).send("Campeonato no encontrado");
+//     } else {
+//       res.json(row);
+//     }
+//   });
+// });
+
+// router.get("/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   if (isNaN(id)) {
+//     res.status(400).send("El id ingresado no es válido.");
+//     return;
+//   }
+//   db.get("SELECT * FROM campeonatos WHERE id = ?", [id], (err, row) => {
+//     if (err) {
+//       console.error(err.message);
+//       res.status(500).send("Error interno del servidor");
+//       return;
+//     }
+//     if (!row) {
+//       res.status(404).send("El campeonato no existe.");
+//       return;
+//     }
+//     const idCampeon = row.id_campeon;
+//     axios
+//       .get(`http://localhost:4000/api/v2/perros/${idCampeon}`)
+//       .then((response) => {
+//         const perro = response.data.data;
+//         res.json({
+//           id: id,
+//           id_campeon: idCampeon,
+//           perro: perro,
+//         });
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         res.status(500).send("Error al obtener los detalles del perro.");
+//       });
+//   });
+// });
+
+
+
+
+function generarConsultaSQL(puntaje) {
+  const cantidadAsteriscos = puntaje / 10; // Dividimos por 10 para obtener la cantidad de asteriscos que se están buscando
+  const asteriscos = "*".repeat(cantidadAsteriscos); // Creamos una cadena de texto con la cantidad exacta de asteriscos
+  return `SELECT id_campeon FROM campeonatos WHERE puntaje LIKE '${asteriscos}'`;
+}
+
+router.get("/:puntaje", (req, res) => {
+  const puntaje = parseInt(req.params.puntaje);
+  if (isNaN(puntaje) || puntaje < 10 || puntaje > 50 || puntaje % 10 !== 0) {
+    res.status(400).send("El puntaje ingresado no es válido.");
+    return;
+  }
+  const consultaSQL = generarConsultaSQL(puntaje);
+  db.all(consultaSQL, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Error interno del servidor");
+    } else {
+      const id_campeones = rows.map(row => row.id_campeon);
+      const perrosPromises = id_campeones.map(id_campeon => axios.get(`http://localhost:4000/api/v2/perros/${id_campeon}`));
+      Promise.all(perrosPromises)
+        .then(responses => {
+          const perrosData = responses.map(response => response.data);
+          const response = {
+            service: "premios",
+            architecture: "microservices",
+            puntaje: puntaje,
+            perrosData: perrosData,
+          };
+          res.json(response);
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).send("Error al obtener los datos de los perros");
+        });
+    }
+  });
+});
+
+
+/* router.get("/:puntaje", (req, res) => {
+  const puntaje = parseInt(req.params.puntaje);
+  if (isNaN(puntaje) || puntaje < 10 || puntaje > 50 || puntaje % 10 !== 0) {
+    res.status(400).send("El puntaje ingresado no es válido.");
+    return;
+  }
+  const consultaSQL = generarConsultaSQL(puntaje);
+  db.all(consultaSQL, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Error interno del servidor");
+    } else {
+      res.json(rows);
+    }
+  });
+}); */
+
+
+// Función que devuelve la consulta SQL según el número ingresado
+// function generarConsultaSQL(numero) {
+//   let puntajeCondicion = "";
+//   switch (numero) {
+//     case 10:
+//       puntajeCondicion = "WHERE puntaje = '*'";
+//       break;
+//     case 20:
+//       puntajeCondicion = "WHERE puntaje = '**'";
+//       break;
+//     case 30:
+//       puntajeCondicion = "WHERE puntaje = '***'";
+//       break;
+//     case 40:
+//       puntajeCondicion = "WHERE puntaje = '****'";
+//       break;
+//     case 50:
+//       puntajeCondicion = "WHERE puntaje = '*****'";
+//       break;
+//     default:
+//       break;
+//   }
+//   return `SELECT premio FROM campeonatos ${puntajeCondicion}`;
+// }
+
+// // Endpoint refactorizado con variables más descriptivas
+// router.get("/:puntaje", (req, res) => {
+//   const puntaje = req.params.puntaje;
+//   const consultaSQL = generarConsultaSQL(puntaje);
+
+//   db.all(consultaSQL, (err, rows) => {
+//     if (err) {
+//       console.error(err.message);
+//       res.status(500).send("Error interno del servidor");
+//     } else {
+//       res.json(rows);
+//     }
+//   });
+// });
+
+
+
 module.exports = router;
